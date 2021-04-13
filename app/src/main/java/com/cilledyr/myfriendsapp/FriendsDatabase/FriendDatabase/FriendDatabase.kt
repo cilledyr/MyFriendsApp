f5c361d071beb868ac1cafe0724457888e068dc8
MacOS <filipus959@gmail.com>
2021-04-13 21:21:18 +0200

    .
    .

--- app/src/main/java/com/cilledyr/myfriendsapp/FriendsDatabase/FriendDatabase/FriendDatabase.kt
+++ app/src/main/java/com/cilledyr/myfriendsapp/FriendsDatabase/FriendDatabase/FriendDatabase.kt
@@ -18,7 +18,10 @@ abstract class FriendDatabase :RoomDatabase() {
 val migration_7_8 = object : Migration(7, 8) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
-            "ALTER TABLE BeFriend ADD COLUMN coordinateX TEXT NOT NULL DEFAULT ''"
+            "ALTER TABLE BeFriend ADD COLUMN coordinateX DOUBLE NOT NULL DEFAULT ''"
+        )
+        database.execSQL(
+            "ALTER TABLE BeFriend ADD COLUMN coordinateY DOUBLE NOT NULL DEFAULT ''"
         )
     }
 }
@@ -29,12 +32,12 @@ val migration_3_6 = object : Migration(3, 6) {
         database.execSQL(
             "CREATE TABLE friends_new " +
                     "(id TEXT NOT NULL, firstName TEXT NOT NULL, lastName TEXT NOT NULL, email TEXT NOT NULL, " +
-                    "phoneNr TEXT NOT NULL, birthday INTEGER NOT NULL, isFavorite INTEGER NOT NULL,coordinateX DOUBLE NOT NULL, coordinateY DOUBLE NOT NULL," +
+                    "phoneNr TEXT NOT NULL, birthday INTEGER NOT NULL, isFavorite INTEGER NOT NULL," +
                     "PRIMARY KEY(id))");
         // Copy the data
         database.execSQL(
             "INSERT INTO friends_new (Id, firstName, lastName, email, phoneNr, birthday, isFavorite) SELECT " +
-                    "Id, firstName, lastName, email, phoneNr, birthday, isFavorite, coordinateX, coordinateY FROM BeFriend");
+                    "Id, firstName, lastName, email, phoneNr, birthday, isFavorite FROM BeFriend");
                     // Remove the old table
                     database.execSQL("DROP TABLE BeFriend");
         // Change the table name to the correct one
